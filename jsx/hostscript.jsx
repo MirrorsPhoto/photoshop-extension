1dc4b9bb50e0fff458d7923c30d4753a5e366aad
jonkofee <jonkofee@icloud.com>
2017-08-26 18:13:31 +0300

    render

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,15 +1,14 @@
-function render(width,height, count) {
-
+function render(width, height, count) {
     try {
         var activeDocument = app.activeDocument;
     } catch (e) {
         alert ('Нет активного документа')
-        return;
+        return false;
     }
 
-    if (activeDocument.height != height || activeDocument.width != width) {
+    if (activeDocument.height.value.toFixed(2) != height || activeDocument.width.value.toFixed(2) != width) {
         alert('Сначала нужно откадрировать фотографию');
-        return;
+        return false;
     }
 
     layerWidth = 15;
@@ -17,8 +16,8 @@ function render(width,height, count) {
 
     if ((layerWidth * layerHeight) < ((count + 0.5) * width * height)) {
         alert(count + " фотографии " + height + "х" + width + " не вмещаются на холст " + layerHeight + "х" + layerWidth);
-        return;
+        return false;
     }
 
-    alert('OK');
+    return true;
 }
