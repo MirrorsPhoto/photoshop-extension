6fdbed839a4096d0ec73a3e87dd76ad357e179ae
jonkofee <jonkofee@icloud.com>
2017-08-27 13:52:18 +0300

    Динамическая подгрузка размеров фото
    Размеры фото теперь грузятся с сервера, а если нет интернета - достаются из localStorage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
-(function () {
-    'use strict';
+function init() {
+    csInterface = new CSInterface();
 
-    var csInterface = new CSInterface();
+    if (!navigator.onLine && !JSON.parse(localStorage.getItem('photo_size'))) {
+        alert('Не удаётся загрузить размеры фото');
+        return false;
+    }
 
-    $('#test').click(function () {
-        var selectedSize = $('#size option:selected');
+    var selectData;
 
-        var width = selectedSize.data('width');
-        var height = selectedSize.data('height');
+    if (navigator.onLine) {
+        $.get({
+            url: 'http://api.jonkofee.ru/photo/size',
+            async: false,
+            success: function (data) {
+                localStorage.setItem('photo_size', JSON.stringify(data.response));
+                selectData = data.response;
+            }
+        })
+    } else {
+        selectData = JSON.parse(localStorage.getItem('photo_size'));
+    }
 
-        var count = $('#count').val();
-        
-        csInterface.evalScript('render(' + width + ', ' + height + ', ' + count + ')');
-    });
+    for (objSize of selectData) {
+        size.innerHTML += '<option data-width="' + objSize.width + '" data-height="' + objSize.height + '" value="' + objSize.id + '">' + objSize.width + 'х' + objSize.height + '</option>'
+    }
+}
 
-}());
-    
+
+$('#test').click(function () {
+    var selectedSize = $('#size option:selected');
+
+    var width = selectedSize.data('width');
+    var height = selectedSize.data('height');
+
+    var count = $('#count').val();
+
+    csInterface.evalScript('render(' + width + ', ' + height + ', ' + count + ')');
+});
