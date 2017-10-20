ec7361702179ac4c1d8d06b862c4bf5b4ea39e1a
jonkofee <jonkofee@icloud.com>
2017-10-20 14:19:04 +0300

    Reconection socket close #1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 import App from './App.vue';
 import router from './router';
 import axios from './axios';
 
-const socket = new WebSocket("ws://localhost:8081");
+const socket = new ReconnectingWebSocket("ws://localhost:8081", undefined, { maxReconnectionDelay: 4000 });
 
 socket.onopen = function() {
   console.log("Соединение установлено.");
